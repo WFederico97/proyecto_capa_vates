6d0b55374ac2a2e026d732b9c87fdf0cbda77a4a
Federico W <wuthrich09@gmail.com>
2023-07-05 16:40:12 -0300

    Minor fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,47 @@ import Register from './components/register/Register.jsx';
 
 import './App.css';
 import { Box, Grid } from '@mui/material';
+import { useEffect } from 'react';
 
 function App() {
+  const isAuth = localStorage.getItem('token')
+  let logoutTimer 
+  const expirationDate = new Date(JSON.parse(localStorage.getItem('expires')))
+
+  const handleLogout = () => { 
+    localStorage.removeItem('token')
+    localStorage.removeItem('expires')
+  }
+  
+  useEffect(() => {
+
+    const autoLogout = () => {
+
+      if (expirationDate) {
+
+        const remainingTime =
+
+         expirationDate.getTime() - new Date().getTime()
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+
+        logoutTimer = setTimeout(() => {
+
+          handleLogout()
+
+        }, remainingTime)
+
+      } else {
+
+        clearTimeout(logoutTimer)
+
+      }
+
+    }
+
+    autoLogout()
+
+  }, [expirationDate])
   return (
     <Box component="div" sx={{ display: "flex", flexDirection: "column", alignContent: "center" }}>
       <header className='App-header'>
--- frontend/src/services/login/login.js
+++ frontend/src/services/login/login.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
-export  const login =  async (data) => {
- const response = await axios.post('http://localhost:8000/auth/login', data)
+export const login = async (data) => {
+    const body = new URLSearchParams(data)
+    const response = await axios.post('http://localhost:8000/auth/login', body)
+    localStorage.setItem('acces_token', response.data.accessToken)
+    const expirationDate = new Date(new Date().getTime() + (1000 * 60 * 15))
+    localStorage.setItem('expires', JSON.stringify(expirationDate.toISOString()))
     return response.data
 
 }
